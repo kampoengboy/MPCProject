3c0f1e86b276179e5f53b63e9b4fe395b54f3659
Michael <mike_visualsoft@yahoo.com>
2016-01-04 14:25:48 +0700

    add

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,19 +10,19 @@
  */
 
 module.exports.bootstrap = function(cb) {
-
+cb();
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  User.update({}, {
-			online: false
-		},
-		function userUpdated(err, users) {
-			if (err) {
-				console.log(err);
-			} else {
-				// console.log(users);
-			}
-			cb();
-		}
-	)
+  // User.update({}, {
+	// 		online: false
+	// 	},
+	// 	function userUpdated(err, users) {
+	// 		if (err) {
+	// 			console.log(err);
+	// 		} else {
+	// 			// console.log(users);
+	// 		}
+	// 		cb();
+	// 	}
+	// )
 };
